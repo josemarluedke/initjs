cf5a48c67edfaa944603b2f6572e61d4981c7f36
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-10-25 21:14:09 -0500

    Changes for dummy app in Rails 4

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = '7eb96180d18be48094cf06cbd9f0f04f66ac7b977010a83bc58a862319650829a7b7a2e7a1053de2ba65c581d2f7c2be0e5fb5676c1b3a2bd1e107a12b90e71a'
+Dummy::Application.config.secret_key_base = 'ec64257c06ec666dc7962011aa72549f8af1b9e02048552938783340dbd43d3d69ddd66e61c436a80feddf5ab6b17cd63e032a2179c586fa9c6d02cf4dde5e09'
